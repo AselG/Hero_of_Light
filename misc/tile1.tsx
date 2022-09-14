5f79f3eca78985627475078b1dabc407198fc81a
AselG <56326356+AselG@users.noreply.github.com>
2022-09-13 21:07:04 -0400

    Add files via upload

---
+++ misc/tile1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tile1" tilewidth="32" tileheight="32" tilecount="1764" columns="42">
+ <image source="../../../../../Downloads/PixelPlatformerSet2v.1.1/main_lev_buildA.png" width="1344" height="1344"/>
+</tileset>
---
+++ misc/tile2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tile2" tilewidth="16" tileheight="16" tilecount="7056" columns="84">
+ <image source="../../../../../Downloads/PixelPlatformerSet2v.1.1/main_lev_buildA.png" width="1344" height="1344"/>
+</tileset>
